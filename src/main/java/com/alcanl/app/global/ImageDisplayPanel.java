ee434a73cb81ba42c6066dea12ef57e3689c2b43
Alican Keçici <alican.kecici@gmail.com>
2023-10-23 17:07:29 +0300

    23.10.2023_commit_2 | webcam_connection_implementation_test

---
+++ src/main/java/com/alcanl/app/global/ImageDisplayPanel.java
@@ -0,0 +1,47 @@
+package com.alcanl.app.global;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ImageDisplayPanel extends JPanel
+{
+    private static final Object BACKGROUND_LOCK = new Object();
+    private BufferedImage background;
+    public ImageDisplayPanel () throws HeadlessException
+    {
+        this.setDoubleBuffered(true); //to avoid flicker
+    }
+
+    public void setBackground (Image newBackground)
+    {
+        synchronized (BACKGROUND_LOCK)
+        {
+            if (background == null)
+            {
+                background = new BufferedImage(newBackground.getWidth(null), newBackground.getHeight(null), BufferedImage.TYPE_INT_RGB);
+            }
+            else if (background.getWidth() != newBackground.getWidth(null) || background.getHeight() != newBackground.getHeight(null))
+            {
+                background.flush();//flush old resources first
+                background = new BufferedImage(newBackground.getWidth(null), newBackground.getHeight(null), BufferedImage.TYPE_INT_RGB);
+            }
+            Graphics graphics = background.createGraphics();
+            graphics.drawImage(newBackground, 0, 0, null);
+        }
+        repaint();
+    }
+
+    @Override
+    public void paint (Graphics g)
+    {
+        super.paint(g);
+        synchronized (BACKGROUND_LOCK)
+        {
+            if (background != null)
+            {
+                g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+            }
+        }
+    }
+}
